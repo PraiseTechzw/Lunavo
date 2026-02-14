302e5534aec846fd76d0fb8276c9952262ee17eb
Praise Masunga <praisesamasunga04@gmail.com>
2026-02-14 13:01:56 +0200

    [autopilot] chore: update files Autopilot-Commit: true Autopilot-Version: 2.2.0 Autopilot-User: 9b06151f-e74d-41b9-af57-d1d258601e9e Autopilot-Signature: 24acd8390469187f7997c3a86a18d3a88757c99a0178b6f4f4accedb0c97c76e

--- lib/auth.ts
+++ lib/auth.ts
@@ -342,7 +342,7 @@ export async function resetPassword(email: string): Promise<{ error: any }> {
   const { error } = await supabase.auth.resetPasswordForEmail(
     email.toLowerCase().trim(),
     {
-      redirectTo: "https://peace.praisetech.tech/auth/reset-password",
+      redirectTo: "peace://auth/reset-password",
     },
   );
   return { error };
