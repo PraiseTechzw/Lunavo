0cc4efba61029a3d48e356f7d4f16779c5652b21
Praise Masunga <praisesamasunga04@gmail.com>
2026-02-14 14:15:27 +0200

    [autopilot] chore: update files Autopilot-Commit: true Autopilot-Version: 2.2.0 Autopilot-User: 9b06151f-e74d-41b9-af57-d1d258601e9e Autopilot-Signature: 84229c5e0870359c1df9669113e26186bbdfc17828928cf53a6c08d55d4f1507

--- web-reset/.next/server/middleware-build-manifest.js
+++ web-reset/.next/server/middleware-build-manifest.js
@@ -2,12 +2,38 @@ self.__BUILD_MANIFEST = {
   "polyfillFiles": [
     "static/chunks/polyfills.js"
   ],
-  "devFiles": [],
+  "devFiles": [
+    "static/chunks/react-refresh.js"
+  ],
   "ampDevFiles": [],
   "lowPriorityFiles": [],
   "rootMainFiles": [],
   "pages": {
-    "/_app": []
+    "/": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/index.js"
+    ],
+    "/_app": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_app.js"
+    ],
+    "/_error": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_error.js"
+    ],
+    "/change-password": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/change-password.js"
+    ],
+    "/reset": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/reset.js"
+    ]
   },
   "ampFirstPages": []
 };
